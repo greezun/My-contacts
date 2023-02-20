99dffbf83a31990087667f2550f81ae0b260bf88
Yevhenii Maslak <greezun@gmail.com>
2023-02-20 16:06:22 +0200

    add contactId

--- app/src/main/java/com/example/mycontacts/vh/Contact.kt
+++ app/src/main/java/com/example/mycontacts/vh/Contact.kt
@@ -2,6 +2,7 @@ package com.example.mycontacts.vh
 import java.io.Serializable
 
 data class Contact(
+    val contactId: Int = 0,
     val avatar: String ="",
     val userName: String="",
     val address: String = ""): Serializable
