2a83abbd66f259bcdf2c50cea523c2e9cbc5df7b
Yevhenii Maslak <greezun@gmail.com>
2023-02-20 15:41:06 +0200

    create viewModel with liveData

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -4,27 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycontacts.databinding.ActivityMainBinding
-import com.example.mycontacts.vh.App
 import com.example.mycontacts.vh.ContactAdapter
-import com.example.mycontacts.vh.ContactGenerator
-
-private lateinit var binding: ActivityMainBinding
-private lateinit var adapter: ContactAdapter
-private val contactGenerator = ContactGenerator()
-
+import com.example.mycontacts.vh.ContactViewModel
+import androidx.activity.viewModels
+import com.example.mycontacts.vh.Contact
+import com.example.mycontacts.vh.ContactActionListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter: ContactAdapter by lazy{
+        ContactAdapter(contactActionListener = object: ContactActionListener{
+            override fun onContactDelete (contact: Contact){
+                contactViewModel.deleteContact(contact)
+            }
+        })
+    }
+    private val contactViewModel: ContactViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val manager = LinearLayoutManager(this)
-        adapter = ContactAdapter()
-        adapter.contactList = contactGenerator.getContacts()
 
         binding.recyclerView.layoutManager = manager
         binding.recyclerView.adapter = adapter
+        setObservers()
+        }
 
+    private fun setObservers() {
+        contactViewModel.contactState.observe(this){
+            adapter.submitList(it.toMutableList())
+        }
     }
+
 }
--- app/src/main/java/com/example/mycontacts/vh/Contact.kt
+++ app/src/main/java/com/example/mycontacts/vh/Contact.kt
@@ -1,3 +1,7 @@
 package com.example.mycontacts.vh
+import java.io.Serializable
 
-data class Contact(val avatar: String, val userName: String, val address: String)
+data class Contact(
+    val avatar: String ="",
+    val userName: String="",
+    val address: String = ""): Serializable
