69714d6cbba40a69ba489ecbb0b4d978458a4d0b
Yevhenii Maslak <greezun@gmail.com>
2023-02-20 16:07:41 +0200

    delete contactId

--- app/src/main/java/com/example/mycontacts/vh/Contact.kt
+++ app/src/main/java/com/example/mycontacts/vh/Contact.kt
@@ -2,7 +2,6 @@ package com.example.mycontacts.vh
 import java.io.Serializable
 
 data class Contact(
-    val contactId: Int = 0,
     val avatar: String ="",
     val userName: String="",
     val address: String = ""): Serializable
