8dd1e6cdb1a9de66f29da0b09e4f2de80ea25503
Yevhenii Maslak <greezun@gmail.com>
2023-03-01 19:36:24 +0200

    dialog

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycontacts.databinding.ActivityMainBinding
 import com.example.mycontacts.vh.ContactAdapter
 import com.example.mycontacts.vh.ContactViewModel
 import androidx.activity.viewModels
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.lifecycleScope
 import com.example.mycontacts.vh.Contact
 import com.example.mycontacts.vh.ContactActionListener
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var addContact: AppCompatTextView
     private lateinit var binding: ActivityMainBinding
     private val adapter: ContactAdapter by lazy {
         ContactAdapter(contactActionListener = object : ContactActionListener {
@@ -27,9 +34,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val manager = LinearLayoutManager(this)
+        addContact = binding.tvAddContact
         binding.recyclerView.layoutManager = manager
         binding.recyclerView.adapter = adapter
         setObservers()
+        onClickListener()
+
+    }
+
+    private fun onClickListener()  {
+        binding.tvAddContact.setOnClickListener{
+            val contactAdder = AddContactDialog()
+            contactAdder.show(supportFragmentManager,"add_contact")
+        }
+    }
+
+    fun showAddContactDialog(v: View){
+        val contactAdder:AddContactDialog = AddContactDialog()
+        contactAdder.show(supportFragmentManager,"add_contact")
     }
 
     private fun setObservers() {
--- app/src/main/java/com/example/mycontacts/vh/App.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mycontacts.vh
-
-import android.app.Application
-
-class App: Application() {
-    val contactGenerator = ContactGenerator()
-}
